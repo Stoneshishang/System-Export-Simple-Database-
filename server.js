939b34fb94cd89ef14fbdc2fb21fcb1c41b5822f
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-02-12 14:24:10 -0500

    add example of proxy and reverse proxy with curl

--- server.js
+++ server.js
@@ -64,3 +64,8 @@ app.get('/withcache/index.html', (req, res) =>{
 app.listen(3001, () =>{
   console.log('Listening on port 3001!');
 })
+
+app.get('/hello', (req, res) =>{
+  console.log(req.headers);
+  res.send('Hello.\n');
+})
